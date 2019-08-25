f54ab6487d61b0cc951bedc4bff4c94d5b3b1767
kylebrown <kylejbrown17@gmail.com>
2019-08-24 22:42:49 -0700

    Major cleanup. I just needed things to be a little simpler in order to move ahead more quickly!

--- test/task_graph_tests.jl
+++ test/task_graph_tests.jl
@@ -1,31 +1,19 @@
 module TaskGraphTests
 
-using LightGraphs, MetaGraphs
+using LightGraphs, MetaGraphs, GraphUtils
 using TaskGraphs
 using Test
 using Logging
+using Gurobi
+
 # Set logging level
 global_logger(SimpleLogger(stderr, Logging.Debug))
 # Define package tests
 function run_tests()
-    # # test array sorting
-    # array = [1.0,2.0,3.0,4.0,5.0]
-    # array = insert_to_sorted_array!(array, 3.5)
-    # @test(array[4] == 3.5)
-    # array = insert_to_sorted_array!(array, 0.5)
-    # @test(array[1] == 0.5)
-    # array = insert_to_sorted_array!(array, 5.5)
-    # @test(array[end] == 5.5)
-
     G = DiGraph(3)
     add_edge!(G,1,2)
     @test get_all_root_nodes(G) == Set([2,3])
 
-    initial_env_state = EnvState(
-        robot_states = [AgentState(x=i,b=100,t=0) for i in 1:10],
-        object_states = [ObjectState(x=i) for i in 11:20]
-        )
-    env_state = copy(initial_env_state)
     @time @testset "TaskGraphs Package Tests" begin
         @time @testset "TaskGraphTests" begin
             N = 4                   # num robots
@@ -48,82 +36,43 @@ function run_tests()
             # populate_lower_time_bound!(task_graph,env_state,env_model,t_lower,length(task_graph.operations))
             # @show t_lower
 
+            N = 20                  # num robots
+            M = 60                  # num delivery tasks
+            # project_spec = construct_random_project_spec(M;max_parents=3,depth_bias=1.0,Δt_min=0,Δt_max=0)
+            project_spec1 = construct_random_project_spec(Int(M/2);max_parents=3,depth_bias=0.25,Δt_min=0,Δt_max=0)
+            project_spec2 = construct_random_project_spec(Int(M/2);max_parents=3,depth_bias=0.25,Δt_min=0,Δt_max=0)
+            project_spec = combine_project_specs([project_spec1, project_spec2])
+
+            delivery_graph = construct_delivery_graph(project_spec,M)
+            r₀,s₀,sₜ,pts = initialize_random_2D_task_graph_env(N,M;d=[400,400])
+            Drs, Dss = cached_pickup_and_delivery_distances(pts[r₀],pts[s₀],pts[sₜ])
+            G = delivery_graph.graph
+            # initialize vector of operation times
+            Δt = zeros(nv(G)) # Δt[j] is the wait time for the object j to appear once all inputs have been satisfied
+            for op in project_spec.operations
+                for id in get_output_ids(op)
+                    Δt[id] = duration(op)
+                end
+            end
+
+            # set initial conditions
+            to0_ = Dict{Int,Float64}()
+            for v in vertices(G)
+                if is_leaf_node(G,v)
+                    to0_[v] = 0.0
+                end
+            end
+            tr0_ = Dict{Int,Float64}()
+            for i in 1:N
+                tr0_[i] = 0.0
+            end
+
+            model = formulate_JuMP_optimization_problem(G,Drs,Dss,Δt,to0_,tr0_,Gurobi.Optimizer;OutputFlag=0);
+            spec = TaskGraphProblemSpec(N,M,G,Drs,Dss,Δt,tr0_,to0_)
         end
         @time @testset "RandomInitializationTests" begin
             construct_random_project_spec(20)
         end
-        # @time @testset "SimpleTransitionTests" begin
-        #     let
-        #         let
-        #             env = nothing
-        #             env_state = copy(initial_env_state)
-        #             initial_state = AgentState(x=1,b=100,t=0,o=-1)
-        #             action = GO(2)
-        #             state = transition(env, initial_state, action)
-        #             @test state.x == action.x
-        #             @test state.b == initial_state.b - 1
-        #             @test state.o == initial_state.o
-        #             @test state.t == initial_state.t + 1
-        #             # collect and deposit object
-        #             action = GO(env_state.object_states[1].x)
-        #             state = transition(env, initial_state, action)
-        #             @test state.x == action.x
-        #             action = COLLECT(1)
-        #             state = transition(env, state, action)
-        #             @test state.o == action.o
-        #             action = DEPOSIT(1)
-        #             state = transition(env, state, action)
-        #             @test state.o == ObjectID()
-        #             action = CHARGE(100)
-        #             state = transition(env, state, action)
-        #             @test state.b == action.b
-        #         end
-        #     end
-        # end
-        # @time @testset "EnvTests" begin
-        #     let
-        #         env = nothing
-        #         env_state = copy(initial_env_state)
-        #         @test length(env_state.action_queue) == length(env_state.robot_states)
-        #         agent_id = 1
-        #         object_id = 1
-        #         action = GO(env_state.object_states[object_id].x)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         @test agent_state.x == action.x
-        #         action = COLLECT(object_id)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         object_state = env_state.object_states[object_id]
-        #         @test agent_state.o == object_id
-        #         @test object_state.r == agent_id
-        #         action = GO(1)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         object_state = env_state.object_states[object_id]
-        #         @test agent_state.x == action.x
-        #         @test object_state.x == agent_state.x
-        #         action = DEPOSIT(object_id)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         object_state = env_state.object_states[object_id]
-        #         @test agent_state.o == ObjectID()
-        #         @test object_state.r == RobotID()
-        #         action = GO_AND_CHARGE(env.charger_ids[1],100)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         @test agent_state.x == action.x
-        #         @test agent_state.b == action.b
-        #         object_id = 2
-        #         action = COLLECT_AND_DELIVER(2,env_state.object_states[object_id].x,agent_state.x)
-        #         env_state = transition(env,env_state,agent_id,action)
-        #         agent_state = env_state.robot_states[agent_id]
-        #         object_state = env_state.object_states[object_id]
-        #         @test agent_state.x == object_state.x == action.x2
-        #         @test agent_state.o == ObjectID()
-        #         @test object_state.r == RobotID()
-        #     end
-        # end
     end
 end
 end
