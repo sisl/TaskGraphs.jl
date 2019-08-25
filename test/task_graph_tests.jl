2d3f2896b765cc6896110c78cbbe7c92a902ca72
kylebrown <kylejbrown17@gmail.com>
2019-08-24 22:44:10 -0700

    Removed a few old tests.

--- test/task_graph_tests.jl
+++ test/task_graph_tests.jl
@@ -16,26 +16,6 @@ function run_tests()
 
     @time @testset "TaskGraphs Package Tests" begin
         @time @testset "TaskGraphTests" begin
-            N = 4                   # num robots
-            M = 10                  # num delivery tasks
-            r₀,s₀,sₜ = initialize_random_2D_task_graph_env(N,M;d=[20,20])
-            D = construct_factory_distance_matrix(r₀,s₀,sₜ)
-
-            project_spec = ProjectSpec()
-            add_operation!(project_spec,construct_operation(1, [1,2,3],[4], 0))
-            add_operation!(project_spec,construct_operation(2, [5,6]  ,[7], 0))
-            add_operation!(project_spec,construct_operation(3, [4,7]  ,[8], 0))
-            add_operation!(project_spec,construct_operation(4, [8,9]  ,[10],0))
-            @test has_edge(project_spec.graph, 1, 3)
-            @test has_edge(project_spec.graph, 2, 3)
-            @test has_edge(project_spec.graph, 3, 4)
-
-            # compute lower bound on task graph
-            # t_lower = fill!(Vector{Int}(undef,nv(task_graph.graph)),typemax(Int))
-            # env_model = HighLevelEnvModel()
-            # populate_lower_time_bound!(task_graph,env_state,env_model,t_lower,length(task_graph.operations))
-            # @show t_lower
-
             N = 20                  # num robots
             M = 60                  # num delivery tasks
             # project_spec = construct_random_project_spec(M;max_parents=3,depth_bias=1.0,Δt_min=0,Δt_max=0)
