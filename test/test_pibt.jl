7d0616f4a95171500b9a6319627d484630243c9d
kylebrown <kylejbrown17@gmail.com>
2020-07-27 16:48:01 -0700

    Saved test_pibt.jl

--- test/test_pibt.jl
+++ test/test_pibt.jl
@@ -1,5 +1,5 @@
 let
-    f = pctapf_problem_4
+    f = pctapf_problem_3
     cost_model = SumOfMakeSpans()
     project_spec, problem_spec, robot_ICs, _, env_graph = f(;cost_function=cost_model,verbose=false)
     solver = NBSSolver(assignment_model = TaskGraphsMILPSolver(GreedyAssignment()))
@@ -21,16 +21,69 @@ let
     @test validate(sched)
 
     solver = PIBTPlanner{NTuple{3,Float64}}()
-    search_env = construct_search_env(planner,deepcopy(sched),base_search_env)
+    search_env = construct_search_env(solver,deepcopy(sched),base_search_env)
     @show get_cost_model(search_env)
     pc_mapf = PC_MAPF(search_env)
 
     num_agents(pc_mapf)
     node  = initialize_root_node(solver,pc_mapf)
     build_env(solver,pc_mapf,node,1)
-    get_initial_solution(pc_mapf)
 
     cache = CRCBS.pibt_init_cache(solver,pc_mapf)
-    pibt!(solver,mapf)
+    is_consistent(cache,pc_mapf)
+    # pibt_step!(solver,pc_mapf,cache,1)
+    set_iteration_limit!(solver,20)
+    set_verbosity!(solver,4)
+    reset_solver!(solver)
+    solution, valid_flag = pibt!(solver,pc_mapf)
 
+    @show vtx_lists = convert_to_vertex_lists(solution)
+    grid_vtxs = map(v->[v[1],v[2]], pc_mapf.env.env.graph.vtxs)
+    paths = map(p->map(v->grid_vtxs[v],p),vtx_lists)
+
+    # @show get_cost(solution)
+    @show convert_to_vertex_lists(solution.route_plan)
+    @test valid_flag
+end
+let
+    cost_model = SumOfMakeSpans()
+    for (i, (f,expected_cost)) in enumerate([
+        (pctapf_problem_1,5),
+        (pctapf_problem_2,8),
+        (pctapf_problem_3,10),
+        (pctapf_problem_4,3),
+        (pctapf_problem_5,4),
+        (pctapf_problem_8,16),
+        ])
+        solver = NBSSolver(assignment_model = TaskGraphsMILPSolver(GreedyAssignment()))
+        let
+            project_spec, problem_spec, robot_ICs, _, env_graph = f(;cost_function=cost_model,verbose=false)
+            project_schedule = construct_partial_project_schedule(
+                project_spec,
+                problem_spec,
+                robot_ICs,
+                )
+            base_search_env = construct_search_env(
+                solver,
+                project_schedule,
+                problem_spec,
+                env_graph
+                )
+            prob = formulate_assignment_problem(solver.assignment_model,base_search_env;
+                optimizer=Gurobi.Optimizer,
+            )
+            sched, _ = solve_assignment_problem!(solver.assignment_model,prob,base_search_env)
+
+            pibt_planner = PIBTPlanner{NTuple{3,Float64}}()
+            search_env = construct_search_env(pibt_planner,deepcopy(sched),base_search_env)
+            pc_mapf = PC_MAPF(search_env)
+            set_iteration_limit!(pibt_planner,50)
+            set_verbosity!(pibt_planner,0)
+            solution, valid_flag = pibt!(pibt_planner,pc_mapf)
+            @show i, f, get_cost(solution)
+            # @show convert_to_vertex_lists(solution.route_plan)
+            @test valid_flag
+            @test expected_cost <= get_cost(solution)
+        end
+    end
 end
