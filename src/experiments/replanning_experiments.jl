8ed1d9eafa18e64ced8690b0f74daa47ab79311c
Kyle Brown <kylejbrown17@gmail.com>
2021-03-26 15:11:02 -0700

    Just launched replanning experiments with varying beta.

--- src/experiments/replanning_experiments.jl
+++ src/experiments/replanning_experiments.jl
@@ -375,7 +375,8 @@ export
     replanning_config_3,
     replanning_config_4,
     replanning_config_5,
-    replanning_config_6
+    replanning_config_6,
+    replanning_config_7
 
 """
     replanning_config_1
@@ -668,6 +669,40 @@ function replanning_config_6()
     product_config_dicts(base_configs,robot_configs,project_configs,stream_configs)
 end
 
+function replanning_config_7()
+    base_configs = [
+        Dict(
+            :warning_time=>0,
+            :commit_threshold=>10,
+            :fallback_commit_threshold=>10,
+            :num_trials => 8,
+            :max_parents => 3,
+            :depth_bias => 0.4,
+            :dt_min => 0,
+            :dt_max => 0,
+            :dt_collect => 0,
+            :dt_deliver => 0,
+            :env_id=>"env_2",
+            )
+    ]
+    robot_configs = [
+        Dict(:N=>30)
+    ]
+    project_configs = [
+        Dict(:num_projects=>30),
+    ]
+    stream_configs = [
+        Dict(:M=>20, :num_unique_projects=>1, :arrival_interval=>20, ),
+
+        Dict(:M=>25, :num_unique_projects=>1, :arrival_interval=>20, ),
+        Dict(:M=>25, :num_unique_projects=>1, :arrival_interval=>30, ),
+
+        Dict(:M=>30, :num_unique_projects=>1, :arrival_interval=>20, ),
+        Dict(:M=>30, :num_unique_projects=>1, :arrival_interval=>30, ),
+        Dict(:M=>30, :num_unique_projects=>1, :arrival_interval=>40, ),
+    ]
+    product_config_dicts(base_configs,robot_configs,project_configs,stream_configs)
+end
 
 export setup_replanning_experiments
 
--- src/path_planners/pibt_planner.jl
+++ src/path_planners/pibt_planner.jl
@@ -88,14 +88,15 @@ function CRCBS.pibt_update_envs!(solver,pc_mapf::PC_MAPF,cache)
     while true
         done = true
         for (i,p) in enumerate(get_paths(solution))
-            v_next = get_next_vtx_matching_agent_id(solution,RobotID(i))
-            if has_vertex(solution.schedule,v_next)
-                # rebuild env to ensure that the goal time is up to date
-                CRCBS.get_envs(cache)[i] = build_env(solver,pc_mapf,solution,node,AgentID(i))
-            end
             env = CRCBS.get_envs(cache)[i]
             v = get_vtx(solution.schedule,env.node_id)
             sp = get_final_state(p)
+            v_next = get_next_vtx_matching_agent_id(solution,RobotID(i))
+            if has_vertex(solution.schedule,v_next) && is_goal(env,sp)
+                # rebuild env to ensure that the goal time is up to date
+                CRCBS.get_envs(cache)[i] = build_env(solver,pc_mapf,solution,node,AgentID(i))
+                env = CRCBS.get_envs(cache)[i]
+            end
             if (v in solution.cache.active_set) && is_goal(env,sp) && CRCBS.is_valid(env,get_goal(env))
                 done = false
                 update_env!(solver,solution,v,p)
